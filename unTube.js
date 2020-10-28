6e465a00efad9c02e48e77ee562150588a3f260b
Taras Shabatyn <taras.shabatyn@eltrino.com>
2020-10-28 03:18:22 +0200

    Un YouTube Me: Beta

---
+++ unTube.js
@@ -0,0 +1,18 @@
+// // let recomendationsList = document.getElementById('items');
+// // recomendationsList.outerHTML = "";
+// // recomendationsList.outerHTML = "";
+// setTimeout(function(){ console.log("hi");}, 3000);
+
+var unTube = function() {
+  var element = document.getElementById('secondary');
+  element.parentElement.removeChild(element);
+};
+
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', unTube);
+} else {
+  setTimeout(unTube, 500);
+}
+
+// var element = document.getElementById('secondary');
+// element.parentElement.removeChild(element);
